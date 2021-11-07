b79ae846f57a8dd9f96eef914caede3f35628a15
Chazita <chazarreta.matias.1138@gmail.com>
2021-11-07 19:00:28 -0300

    new features and fixes

--- packages/api/src/recipe/recipe.service.ts
+++ packages/api/src/recipe/recipe.service.ts
@@ -40,7 +40,7 @@ export class RecipeService {
   }
 
   async getRecipeWithPagination(query: PaginationParams) {
-    const page = query.page || 1;
+    const page = Number(query.page) || 1;
     const skip = (page - 1) * 10;
 
     const data = await this.recipeRepository.findAndCount({
@@ -52,47 +52,103 @@ export class RecipeService {
     return { data, page };
   }
 
+  async getRecipeWithPaginationUser(query: PaginationParams, user: User) {
+    const page = Number(query.page) || 1;
+    const skip = (page - 1) * 10;
+
+    const data = await this.recipeRepository
+      .findAndCount({
+        order: { id: 'DESC' },
+        take: 10,
+        skip,
+        relations: ['createdBy'],
+        loadRelationIds: true,
+        where: { createdBy: user.id },
+      })
+      .catch((error) => console.log(error));
+
+    if (data) return { data, page };
+  }
+
+  async getRecipeForEdit(id: number, user: User) {
+    const recipe = await this.recipeRepository
+      .findOne(id, {
+        relations: ['createdBy', 'ingredients', 'steps'],
+        where: { createdBy: user.id },
+      })
+      .catch((error) => {
+        throw new HttpException(error.toString(), HttpStatus.BAD_REQUEST);
+      });
+
+    if (!recipe) {
+      throw new HttpException(
+        "The recipe doesn't exist or you don't have the requirements to modified it",
+        HttpStatus.BAD_REQUEST,
+      );
+    }
+
+    return recipe;
+  }
+
   async createRecipe(recipe: CreateRecipeDTO, user: User) {
     try {
       const newRecipe = this.recipeRepository.create(recipe);
       newRecipe.createdBy = user;
-      const saveRecipe = await this.recipeRepository.save(newRecipe);
-
-      const newIngredients = this.ingredientRepository.create(
-        recipe.ingredients,
-      );
-      const newSteps = this.stepRepository.create(recipe.steps);
-
-      newIngredients.map((value) => {
-        value.recipes = saveRecipe;
-      });
-      newSteps.map((value) => (value.recipes = saveRecipe));
-
-      await this.ingredientRepository.save(newIngredients);
-      await this.stepRepository.save(newSteps);
+      await this.recipeRepository.save(newRecipe);
     } catch (error) {
-      throw new HttpException(error.toString(), HttpStatus.NOT_IMPLEMENTED);
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
   async updateRecipe(recipe: UpdateRecipeDTO) {
     try {
       await this.recipeRepository.save(recipe);
-      await this.ingredientRepository.save(recipe.ingredients);
-      await this.stepRepository.save(recipe.steps);
     } catch (error) {
-      throw new HttpException(error.toString(), HttpStatus.NOT_MODIFIED);
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 
-  async deleteRecipe(id: number) {
+  async deleteRecipe(id: number, user: User) {
     try {
-      const recipe = await this.getRecipeById(id);
-      await this.ingredientRepository.remove(recipe.ingredients);
-      await this.stepRepository.remove(recipe.steps);
-      await this.recipeRepository.remove(recipe);
+      const recipe = await this.recipeRepository.findOne(id, {
+        relations: ['createdBy', 'ingredients', 'steps'],
+      });
+      if (recipe.createdBy.id === user.id) {
+        await this.recipeRepository.remove(recipe);
+      }
     } catch (error) {
-      throw new HttpException(error.toString(), HttpStatus.NOT_IMPLEMENTED);
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async deleteIngredient(ingredients: Ingredient[]) {
+    try {
+      await this.ingredientRepository.remove(ingredients);
+    } catch (error) {
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
+    }
+  }
+
+  async deleteSteps(steps: Step[]) {
+    try {
+      await this.stepRepository.remove(steps);
+    } catch (error) {
+      throw new HttpException(
+        error.toString(),
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
